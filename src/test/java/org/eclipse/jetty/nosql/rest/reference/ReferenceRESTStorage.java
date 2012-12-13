120dc0a53816af5b9d246d5781f815a92c9ca543
Tomi Takussaari <tomi.takussaari@gmail.com>
2012-12-13 23:24:51 +0200

    ReferenceRESTStorage to check/set request/response headers bit like a riak does

--- src/test/java/org/eclipse/jetty/nosql/rest/reference/ReferenceRESTStorage.java
+++ src/test/java/org/eclipse/jetty/nosql/rest/reference/ReferenceRESTStorage.java
@@ -4,6 +4,8 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -12,6 +14,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 
 import org.eclipse.jetty.nosql.rest.EmbeddedJettyServer;
 import org.eclipse.jetty.servlet.ServletHolder;
@@ -27,36 +30,74 @@ import com.sun.jersey.spi.resource.Singleton;
 @Path("/session")
 public class ReferenceRESTStorage {
 
-    private final Map<String, String> restSessions = Collections.synchronizedMap(new HashMap<String, String>());
+    private final Map<String, ValueWithVclock> restSessions = Collections.synchronizedMap(new HashMap<String, ValueWithVclock>());
+
+    private static class ValueWithVclock {
+        ValueWithVclock(String data, String header) {
+            this.value = data;
+            this.vclock = header;
+        }
+
+        String value;
+        String vclock;
+
+    }
 
     @Path("/sessions/{sessionId}")
     @PUT
-    public void put(@PathParam("sessionId") String sessionId, String data) {
-        restSessions.put(sessionId, data);
+    public void put(@PathParam("sessionId") String sessionId, String data, @Context HttpServletRequest request) {
+        if (restSessions.containsKey(sessionId)) {
+            putWithVclock(sessionId, data, request);
+        } else {
+            System.out.println("NOTE, Session already exists: "+sessionId);
+            post(sessionId, data);
+        }
+
+    }
+
+    private void putWithVclock(String sessionId, String data, HttpServletRequest request) {
+        String vclock = request.getHeader("X-Riak-Vclock");
+
+        if (vclock == null) {
+            vclock = "1";
+            System.out.println("WARNING, NO VCLOCK SPECIFIED!");
+        }
+        ValueWithVclock vwc = new ValueWithVclock(data, plusByOne(vclock));
+        restSessions.put(sessionId, vwc);
     }
 
     @Path("/sessions/{sessionId}")
     @POST
     public void post(@PathParam("sessionId") String sessionId, String data) {
-        System.out.println("POST SessionId: " + sessionId + "IS "+restSessions.get(sessionId));
-        restSessions.put(sessionId, data);
+        if (restSessions.containsKey(sessionId)) {
+            throw new RuntimeException("Key already exists!");
+        } else {
+            restSessions.put(sessionId, new ValueWithVclock(data, "1"));
+        }
     }
 
     @Path("/sessions/{sessionId}")
     @GET
-    public String get(@PathParam("sessionId") String sessionId) {
+    public String get(@PathParam("sessionId") String sessionId, @Context HttpServletResponse response) {
         if (restSessions.containsKey(sessionId)) {
-            System.out.println("GET SessionId: " + sessionId + "IS "+restSessions.get(sessionId));
-            return restSessions.get(sessionId);
+            ValueWithVclock vwc = restSessions.get(sessionId);
+            response.setHeader("X-Riak-Vclock", vwc.vclock);
+            return vwc.value;
         } else {
             throw new NotFoundException();
         }
 
     }
 
+    private String plusByOne(String vclock) {
+        Integer i = Integer.valueOf(vclock);
+        i++;
+        return i.toString();
+    }
+
     @Path("/sessions/{sessionId}")
     @DELETE
-    public void delete(@PathParam("sessionId") String sessionId) {
+    public void delete(@PathParam("sessionId") String sessionId, @Context HttpServletRequest request) {
         if (restSessions.containsKey(sessionId)) {
             restSessions.remove(sessionId);
         } else {
